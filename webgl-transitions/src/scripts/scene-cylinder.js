471160c2b18ab5d09ef361a130e0a6763ef27ef1
Andrew Alvarado <andrew@activetheory.net>
2026-05-08 13:58:59 -0600

    Updated textures to test transitions

--- webgl-transitions/src/scripts/scene-cylinder.js
+++ webgl-transitions/src/scripts/scene-cylinder.js
@@ -26,7 +26,7 @@ export function init(renderer) {
   camera.position.z = 5;
 
   // --- Background quad (screen-space, always behind) ---
-  const bgTexture = new THREE.TextureLoader().load('/textures/scene3.png');
+  const bgTexture = new THREE.TextureLoader().load('/textures/scene3.jpg');
   bgTexture.colorSpace = THREE.SRGBColorSpace;
 
   const bgMaterial = new THREE.ShaderMaterial({
